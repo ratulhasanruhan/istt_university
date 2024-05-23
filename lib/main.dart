717f7790a347fc6f4221e262fc443ddf3f1aa736
Ratul Hasan Ruhan <ratulhasan1644@gmail.com>
2024-05-23 14:04:08 +0600

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:istt_university/util/colors.dart';
 import 'package:istt_university/util/constants.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
