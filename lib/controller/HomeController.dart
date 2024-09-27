57b5db8c5c932ec7e4dc76647160018e554a0f17
Ratul Hasan Ruhan <ratulhasan1644@gmail.com>
2024-09-28 02:28:30 +0600

    class_card.dart

--- lib/controller/HomeController.dart
+++ lib/controller/HomeController.dart
@@ -4,6 +4,8 @@ import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 class HomeController extends GetxController{
   PersistentTabController persistentTabController = PersistentTabController(initialIndex: 0);
 
+  var selectedDate = DateTime.now().obs;
+
   void changeTabIndex(int index) async {
     Get.focusScope?.unfocus();
     persistentTabController.index = index;
--- lib/view/Home.dart
+++ lib/view/Home.dart
@@ -1,6 +1,11 @@
+import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
 import 'package:istt_university/util/colors.dart';
+import '../controller/HomeController.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +15,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final homeController = Get.put(HomeController());
 
   var box = Hive.box('user');
 
@@ -30,8 +37,7 @@ class _HomeState extends State<Home> {
             height: 8,
           ),
           Container(
-            height: 100,
-            padding: EdgeInsets.all(12),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
             decoration: BoxDecoration(
               color: primaryColor,
               borderRadius: BorderRadius.circular(12),
@@ -39,10 +45,187 @@ class _HomeState extends State<Home> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4),
+                          child: SvgPicture.asset(
+                            'assets/academic.svg',
+                          ),
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "CSE",
+                              style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              "BATCH: ",
+                              style: GoogleFonts.roboto(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              "SECTION: ",
+                              style: GoogleFonts.roboto(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(12),
+                      child: InkWell(
+                        onTap: (){},
+                        borderRadius: BorderRadius.circular(12),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset(
+                            'assets/edit.svg',
+                            height: 16,
+                            width: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+
+          EasyDateTimeLine(
+            initialDate: DateTime.now(),
+            onDateChange: (selectedDate) {
+              homeController.selectedDate.value = selectedDate;
+            },
+            activeColor: primaryColor,
+          ),
+
+          SizedBox(
+            height: 24,
+          ),
+
+          Container(
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 16, top: 10, right: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          "Course Name",
+                          style: GoogleFonts.roboto(
+                            fontSize: 20,
+                            shadows: [
+                              Shadow(
+                                  color: Colors.white,
+                                  offset: Offset(0, -4))
+                            ],
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.white,
+                            decorationThickness: 1.7,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ),
+
+                      Text(
+                        "Room: #412",
+                        style: GoogleFonts.roboto(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+
+                      Text(
+                        "Teacher: Md Ashikur Rahman",
+                        style: GoogleFonts.roboto(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Note: Take your laptop",
+                        style: GoogleFonts.roboto(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Divider(
+                  color: Colors.white,
+                  thickness: 1.7,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(),
+                      Text(
+                        "8:00 AM - 9:30 AM",
+                        style: GoogleFonts.roboto(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SvgPicture.asset(
+                        'assets/notification.svg',
+                      ),
+                    ],
+                  ),
+                )
 
               ],
             ),
-          )
+          ),
+
         ],
       ),
     );
